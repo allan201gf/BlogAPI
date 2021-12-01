a7af82d8b83ab3a924affe794e6949ec45adc023
allan201gf <allan201gf@gmail.com>
2021-12-01 20:00:23 -0300

    SwaggerConfig Refactoring

--- src/main/java/br/com/allangf/BlogAPI/rest/config/SwaggerConfig.java
+++ src/main/java/br/com/allangf/BlogAPI/rest/config/SwaggerConfig.java
@@ -22,8 +22,7 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_12)
                 .useDefaultResponseMessages(false)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(List.of(securityContext()))
                 .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -59,7 +58,7 @@ public class SwaggerConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
